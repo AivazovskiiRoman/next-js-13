577a53488c1a474da744b521fd676baf3fa2ec28
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-06-02 11:47:06 -0700

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'About',
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam error
+        nemo quibusdam numquam et quia praesentium recusandae quod? Nostrum, non
+        cupiditate. Quod est optio ut ab alias deleniti voluptas facilis.
+      </p>
+    </>
+  );
+};
+
+export default AboutPage;
