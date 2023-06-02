7e8ed9f6af04ab707186e84bfad1bb690f61475a
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-06-02 11:47:17 -0700

    added team page

---
+++ app/about/team/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Team',
+};
+
+const TeamPage = () => {
+  return (
+    <>
+      <h1>Team Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        perspiciatis commodi, non, illum ipsam nulla sit vero soluta odit unde
+        beatae quas quam iusto veniam consequuntur. Sed a temporibus debitis.
+      </p>
+    </>
+  );
+};
+
+export default TeamPage;
