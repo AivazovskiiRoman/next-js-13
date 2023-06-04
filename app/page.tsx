12a41aadd16c91eb51a770117b1180d23cd7a098
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-06-04 15:04:30 -0700

    updated Home page with courses

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
-import Link from 'next/link';
+import Courses from './components/Courses';
 
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
-      <ul className="inline">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, dolorum
-        non impedit, reprehenderit itaque saepe, nesciunt quidem optio
-        blanditiis libero repellendus repudiandae amet eligendi quisquam
-        consectetur animi a veniam deserunt.
-      </p>
+      <h1 className="text-xl font-semibold mb-5">Courses</h1>
+      <Courses />
     </>
   );
 };
