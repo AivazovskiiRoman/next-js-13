96c8ff1378b0ca10d572494628fa85580f4e54c1
Roman Himadinov <aivazovskiiroman@gmail.com>
2023-06-07 14:47:18 -0700

    Search endpoint refactoring

--- app/api/courses/search/route.ts
+++ app/api/courses/search/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 import data from '../data.json';
 
+const { courses } = data;
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('query');
-  const courses = data.courses;
 
   const filteredCourses = courses.filter((course) =>
-    course.title.toLowerCase().includes(query?.toLowerCase() || '')
+    course.title.toLowerCase().includes(query?.toLowerCase() ?? '')
   );
 
   return NextResponse.json(filteredCourses);
